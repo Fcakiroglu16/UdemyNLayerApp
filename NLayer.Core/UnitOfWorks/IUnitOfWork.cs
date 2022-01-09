d79292894d860df1885f829dea783125b1f78920
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-01-10 02:55:25 +0300

    Core : IUnitOfWork

---
+++ NLayer.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.UnitOfWorks
+{
+    public interface IUnitOfWork
+    {
+        Task CommitAsync();
+        void Commit();
+
+
+    }
+}
