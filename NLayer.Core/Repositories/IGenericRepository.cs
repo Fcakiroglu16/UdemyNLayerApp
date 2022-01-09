e659fcd4545aed8c9c741a6ca1d0db97b4eca929
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-01-10 02:15:11 +0300

    Core : IGenericRepository

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);  
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        
+    }
+}
