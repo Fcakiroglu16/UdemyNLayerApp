17c15d2968bd6e009a90cebc49c72e0b2a447e30
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-01-11 02:12:20 +0300

    Service : Mapping

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -5,6 +5,8 @@ using NLayer.Core.UnitOfWorks;
 using NLayer.Repository;
 using NLayer.Repository.Repositories;
 using NLayer.Repository.UnitOfWorks;
+using NLayer.Service.Mapping;
+using NLayer.Service.Services;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,8 +20,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
-
+builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+builder.Services.AddAutoMapper(typeof(MapProfile));
 
 
 builder.Services.AddDbContext<AppDbContext>(x =>
---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using NLayer.Core;
+using NLayer.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile :Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature,ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+        }
+    }
+}
