9cfb075dd1946a480274ba0d488d3d0188da4de3
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-01-11 00:07:39 +0300

    Repository : UnitOfWork

---
+++ NLayer.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
