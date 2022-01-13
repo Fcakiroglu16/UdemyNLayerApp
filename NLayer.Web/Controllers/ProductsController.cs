9cce0789dada491bc8b1ccfa8369ed6ed28ca074
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-01-13 04:39:40 +0300

    API : Update-1

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -58,5 +58,42 @@ namespace NLayer.Web.Controllers
             return View();
         }
 
+
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var product = await _services.GetByIdAsync(id);
+
+
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoryiesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoryiesDto, "Id", "Name",product.CategoryId);
+
+            return View(_mapper.Map<ProductDto>(product));
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            if(ModelState.IsValid)
+            {
+
+                await _services.UpdateAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoryiesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoryiesDto, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+
+        }
+
     }
 }
