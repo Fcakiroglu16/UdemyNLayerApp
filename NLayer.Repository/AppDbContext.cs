e6c5c4a453f9276158cf282dac7fde42fa190870
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-01-13 05:30:24 +0300

    MVC/API : CreatedDate/UpdatedDate

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -15,6 +15,74 @@ namespace NLayer.Repository
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.Entity)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+
+
+                    }
+                }
+
+
+            }
+
+
+            return base.SaveChanges();
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if(item.Entity is BaseEntity  entityReference)
+                {
+                    switch(item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreatedDate=DateTime.Now;
+                                break;
+                            }
+                            case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreatedDate).IsModified = false;
+
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+
+
+                    }
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
